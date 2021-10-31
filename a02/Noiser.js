2cfc6a3ab42ba5b4757f8fdfdaf8859c2c226f30
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-10-31 17:00:38 +0100

    a02: draw vectors instead of pixels

--- a02/Noiser.js
+++ a02/Noiser.js
@@ -1,37 +1,34 @@
 // eslint-disable-next-line no-unused-vars
 class Noiser {
-  constructor(image, transparency, increment) {
+  constructor(image) {
     this.img = image;
-    this.trans = transparency;
-    this.inc = increment;
   }
 
   get image() {
     return this.img;
   }
 
-  setPixels() {
-    this.img.loadPixels();
+  setPixels(increment, transparency, scale) {
     let yOff = 0;
 
     for (let y = 0; y < this.img.height; y++) {
       let xOff = 0;
 
       for (let x = 0; x < this.img.width; x++) {
-        let index = (x + y * this.img.width) * 4;
         let rand = noise(xOff, yOff) * 255;
-        const [r, g, b, brightness] = color(rand, this.trans).levels;
 
-        this.img.pixels[index + 0] = r;
-        this.img.pixels[index + 1] = g;
-        this.img.pixels[index + 2] = b;
-        this.img.pixels[index + 3] = brightness;
+        const vector = p5.Vector.fromAngle(PI / 6);
+        fill(rand);
+        stroke(0);
+        push();
+        translate(x * scale, y * scale);
+        rotate(vector.heading());
+        line(0, 0, scale, 0);
+        pop();
 
-        xOff += this.inc;
+        xOff += increment;
       }
-      yOff += this.inc;
+      yOff += increment;
     }
-
-    this.image.updatePixels();
   }
 }
--- a02/sketch.js
+++ a02/sketch.js
@@ -1,7 +1,10 @@
-const cWidth = 500;
-const cHeight = 500;
-const increment = 0.01;
+const cWidth = 200;
+const cHeight = 200;
+const increment = 0.1;
 const transparency = 255;
+const scale = 10;
+let cols, rows;
+let fr;
 
 // eslint-disable-next-line no-unused-vars
 function setup() {
@@ -9,11 +12,17 @@ function setup() {
   console.log(`Canvas: ${width} x ${height}`);
   pixelDensity(1);
 
-  const noiser = new Noiser(createImage(cWidth, cHeight), transparency, increment);
+  cols = floor(width / scale);
+  rows = floor(height / scale);
+  fr = createP('');
+}
 
-  noiser.setPixels();
-  const image1 = noiser.image;
-  console.log('IMAGE', image1);
+// eslint-disable-next-line no-unused-vars
+function draw() {
+  const noiser = new Noiser(createImage(cols, rows));
 
-  image(image1, 0, 0);
+  noiser.setPixels(transparency, increment, scale);
+  image(noiser.image, 0, 0);
+
+  fr.html(floor(frameRate()));
 }
