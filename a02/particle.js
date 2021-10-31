612996e6153ed4af3e1bffa5a2bb2f08aea19452
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-10-31 19:34:41 +0100

    a02: let particles follow flowfield

--- a02/particle.js
+++ a02/particle.js
@@ -1,17 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 function Particle() {
   this.pos = createVector(random(width), random(height));
-  this.velo = p5.Vector.random2D();
+  this.velo = createVector(0, 0);
   this.accel = createVector(0, 0);
+  this.maxSpeed = 4;
 
   this.update = function () {
     this.velo.add(this.accel);
+    this.velo.limit(this.maxSpeed);
     this.pos.add(this.velo);
     this.accel.mult(0); // reset acceleration to 0
   };
 
   this.applyForce = function (force) {
     this.accel.add(force);
+  };
+
+  this.follow = function (vectors, scale, cols) {
+    const x = floor(this.pos.x / scale);
+    const y = floor(this.pos.y / scale);
+    const idx = x + y * cols;
+    this.applyForce(vectors[idx]);
   };
 
   this.show = function () {
@@ -23,6 +32,6 @@ function Particle() {
     if (this.pos.x > width) this.pos.x = 0;
     if (this.pos.x < 0) this.pos.x = width;
     if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.x < 0) this.pos.y = height;
+    if (this.pos.y < 0) this.pos.y = height;
   };
 }
--- a02/sketch.js
+++ a02/sketch.js
@@ -1,12 +1,14 @@
 const cWidth = 200;
 const cHeight = 200;
-const inc = 0.01;
+const inc = 0.1;
 const scale = 10;
 let cols, rows;
 let fr;
 
 const particles = [];
 const particleAmount = 100;
+
+let flowfield;
 
 // eslint-disable-next-line no-unused-vars
 function setup() {
@@ -17,6 +19,8 @@ function setup() {
   cols = floor(width / scale);
   rows = floor(height / scale);
   fr = createP('');
+
+  flowfield = new Array(cols * rows);
 
   for (let idx = 0; idx < particleAmount; idx++) {
     particles[idx] = new Particle();
@@ -33,24 +37,31 @@ function draw() {
     let xOff = 0;
 
     for (let x = 0; x < cols; x++) {
+      const index = x + y * cols;
       let angle = noise(xOff, yOff, zOff) * TWO_PI;
-
       const vector = p5.Vector.fromAngle(angle);
+      vector.setMag(0.01);
+      flowfield[index] = vector;
       xOff += inc;
 
       stroke(0, 100);
+      strokeWeight(1);
+
       push();
       translate(x * scale, y * scale);
       rotate(vector.heading());
       line(0, 0, scale, 0);
       pop();
     }
+
     yOff += inc;
-    // zOff += inc / 10.0;
+    zOff += inc / 10.0;
   }
 
+  strokeWeight(2);
   particles.map(particle => {
     particle.update();
+    particle.follow(flowfield, scale, cols);
     particle.edges();
     particle.show();
   });
