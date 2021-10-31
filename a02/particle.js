10b9af78a425881f0806ef35b46b79924dbc4179
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-10-31 23:01:03 +0100

    a02: draw lines instead of points

--- a02/particle.js
+++ a02/particle.js
@@ -1,9 +1,10 @@
 // eslint-disable-next-line no-unused-vars
-function Particle() {
+function Particle(maxSpeed) {
   this.pos = createVector(random(width), random(height));
   this.velo = createVector(0, 0);
   this.accel = createVector(0, 0);
-  this.maxSpeed = 4;
+  this.maxSpeed = maxSpeed;
+  this.prevPos = this.pos.copy();
 
   this.update = function () {
     this.velo.add(this.accel);
@@ -24,15 +25,34 @@ function Particle() {
   };
 
   this.show = function () {
-    stroke(0);
-    strokeWeight(2);
-    point(this.pos.x, this.pos.y);
+    stroke(0, 5);
+    strokeWeight(1);
+    // point(this.pos.x, this.pos.y);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    this.updatePrev();
+  };
+
+  this.updatePrev = function () {
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
   };
 
   this.edges = function () {
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
   };
 }
--- a02/sketch.js
+++ a02/sketch.js
@@ -1,10 +1,11 @@
-const cWidth = 200; // canvas width
-const cHeight = 200; // canvas height
+const cWidth = 500; // canvas width
+const cHeight = 500; // canvas height
 const inc = 0.08; // how often the noise field should update ~[0.01 - 0.8]
-const scale = 10; // length of each vector ~[4 - 20]
-const strength = 0.1; // "gravitational" force of the vectors on dots ~[0.01 - 10]
+const scale = 5; // length of each vector ~[4 - 20]
+const strength = 1; // "gravitational" force of the vectors on dots ~[0.01 - 10]
 const chaosFactor = 1; // the higher, the more inconsistent the vector angles get ~[0.1 - 4]
-const particleAmount = 200; // amounts of particles on the canvas
+const particleAmount = 500; // amounts of particles on the canvas
+const particleMaxSpeed = 4; // maximum particle travel speed ~[1- 10]
 let cols, rows;
 let flowfield;
 let zOff = 0;
@@ -17,6 +18,7 @@ function setup() {
   createCanvas(cWidth, cHeight);
   console.log(`Canvas: ${width} x ${height}`);
   pixelDensity(1);
+  noiseDetail(64);
 
   cols = floor(width / scale);
   rows = floor(height / scale);
@@ -25,14 +27,14 @@ function setup() {
   flowfield = new Array(cols * rows);
 
   for (let idx = 0; idx < particleAmount; idx++) {
-    particles[idx] = new Particle();
+    particles[idx] = new Particle(particleMaxSpeed);
   }
+
+  background(255);
 }
 
 // eslint-disable-next-line no-unused-vars
 function draw() {
-  background(255);
-
   let yOff = 0;
   for (let y = 0; y < rows; y++) {
     let xOff = 0;
@@ -48,11 +50,11 @@ function draw() {
       stroke(0, 50);
       strokeWeight(1);
 
-      push();
-      translate(x * scale, y * scale);
-      rotate(vector.heading());
-      line(0, 0, scale, 0);
-      pop();
+      // push();
+      // translate(x * scale, y * scale);
+      // rotate(vector.heading());
+      // line(0, 0, scale, 0);
+      // pop();
     }
 
     yOff += inc;
