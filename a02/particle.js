6df69d27842f58f3de67c5acaee350f17127043b
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-03 17:29:11 +0100

    a02: setup particles via config

--- a02/particle.js
+++ a02/particle.js
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-function Particle(image, maxVelocity, size, downforce) {
-  this.size = size;
+function Particle({ image, size, chaosFactor, strength, downforce, maxVelocity }) {
   this.image = image;
+  this.size = size;
   this.position = createVector(random(width), random(height));
   this.prevPosition = this.position.copy();
   this.velocity = createVector(0, 0);
   this.acceleration = createVector(0, 0);
+  this.chaosFactor = chaosFactor;
+  this.strength = strength;
   this.gravitationVector = p5.Vector.fromAngle(random(90, 110) * (PI / 180)).setMag(downforce / 10);
   this.rotationAngle = floor(random(0, 180));
   this.hasInversRotation = random(0, 1) > 0.5;
@@ -23,27 +25,12 @@ function Particle(image, maxVelocity, size, downforce) {
     this.prevPosition.y = this.position.y;
   };
 
-  this.applyForce = function (forceVector) {
+  this.follow = function (zOff) {
+    const angle = noise(this.position.x, this.position.y, zOff) * TWO_PI * this.chaosFactor;
+    const forceVector = p5.Vector.fromAngle(angle).setMag(this.strength);
+
     this.acceleration.add(this.gravitationVector);
     this.acceleration.add(forceVector);
-  };
-
-  this.follow = function (zOff, strength, chaosFactor) {
-    const angle = noise(this.position.x, this.position.y, zOff) * TWO_PI * chaosFactor;
-    const forceVector = p5.Vector.fromAngle(angle).setMag(strength);
-    this.applyForce(forceVector);
-  };
-
-  this.show = function (graphic) {
-    graphic.push();
-    graphic.translate(this.position.x, this.position.y);
-    graphic.rotate(this.rotationAngle * (PI / 180));
-    this.rotationAngle += random(0, this.hasInversRotation ? -1 : 1);
-    graphic.imageMode(CENTER);
-    graphic.image(this.image, 0, 0, this.size, (this.image.height * this.size) / this.image.width);
-    graphic.pop();
-
-    this.updatePrev();
   };
 
   this.edges = function () {
@@ -63,5 +50,17 @@ function Particle(image, maxVelocity, size, downforce) {
       this.position.y = height;
       this.updatePrev();
     }
+  };
+
+  this.show = function (graphic) {
+    graphic.push();
+    graphic.translate(this.position.x, this.position.y);
+    graphic.rotate(this.rotationAngle * (PI / 180));
+    this.rotationAngle += random(0, this.hasInversRotation ? -1 : 1);
+    graphic.imageMode(CENTER);
+    graphic.image(this.image, 0, 0, this.size, (this.image.height * this.size) / this.image.width);
+    graphic.pop();
+
+    this.updatePrev();
   };
 }
--- a02/sketch.js
+++ a02/sketch.js
@@ -4,25 +4,27 @@ const fontSize = 16; // framerate font size
 let showFrameRate = false; // draw framerate on canvas (or stop drawing)
 console.log('Display the framerate by setting "showFrameRate: true" in the console.');
 
-const inc = 10; // how often the noise position should update ~[1 - 15]
-const strength = 0.2; // "gravitational" force of the vectors on dots ~[0.1 - 10]
-const chaosFactor = 2; // the higher, the more inconsistent the vector angles get ~[0.1 - 4]
-const particleAmount = 100; // amounts of particles on the canvas ~[100]
-const particleMaxSpeed = 1; // maximum particle travel speed ~[1- 10]
-const particleThickness = 40; // line thickness of particles ~[1 - 20]
-const particleDownforce = 1; // downforce on particles aka gravitation ~[0.1 - 5]
+const leafParticleConfig = {
+  zOff: 0,
+  inc: 10, // how often the noise position should update ~[1 - 15]
+  image: null,
+  size: 40, // line thickness of particles ~[1 - 20]
+  amount: 100, // amounts of particles on the canvas ~[100]
+  chaosFactor: 2, // the higher, the more inconsistent the vector angles get ~[0.1 - 4]
+  strength: 0.2, // "gravitational" force of the vectors on dots ~[0.1 - 10]
+  downforce: 1, // downforce on particles aka gravitation ~[0.1 - 5]
+  maxVelocity: 1, // maximum particle travel speed ~[1- 10]
+};
 
-let zOff = 0;
-let particles = new Array(particleAmount).fill(null);
 let forestImg;
 let roomImg;
-let leafImg;
+let leafParticles = new Array(leafParticleConfig.amount).fill(null);
 
 // eslint-disable-next-line no-unused-vars
 function preload() {
   forestImg = loadImage('img/forest.jpg');
   roomImg = loadImage('img/room.png');
-  leafImg = loadImage('img/leaf.png');
+  leafParticleConfig.image = loadImage('img/leaf.png');
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -33,7 +35,7 @@ function setup() {
   noiseDetail(64);
   imageMode(CORNER);
 
-  particles = particles.map(() => new Particle(leafImg, particleMaxSpeed, particleThickness, particleDownforce));
+  leafParticles = leafParticles.map(() => new Particle(leafParticleConfig));
 }
 
 function drawFrameRate() {
@@ -47,14 +49,14 @@ function drawFrameRate() {
   image(graphic, 0, 0);
 }
 
-function drawParticles() {
-  zOff += inc / 1000.0;
+function drawLeafParticles() {
+  leafParticleConfig.zOff += leafParticleConfig.inc / 1000.0;
 
   const particleGraphic = createGraphics(cWidth, cHeight);
   particleGraphic.colorMode(HSB, 255);
 
-  particles.map(particle => {
-    particle.follow(zOff, strength, chaosFactor);
+  leafParticles.map(particle => {
+    particle.follow(leafParticleConfig.zOff);
     particle.update();
     particle.edges();
     particle.show(particleGraphic);
@@ -66,7 +68,7 @@ function drawParticles() {
 // eslint-disable-next-line no-unused-vars
 function draw() {
   image(forestImg, 0, 0, width, (forestImg.height * width) / forestImg.width);
-  image(drawParticles(), 0, 0);
+  image(drawLeafParticles(), 0, 0);
   image(roomImg, 0, 0, width, (roomImg.height * width) / roomImg.width);
 
   if (showFrameRate) {
