5bd5d903f57f559c5b5a6cc0af7bfd55c58ad67c
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-03 16:46:51 +0100

    a02: cleanup sketch.js

--- a02/sketch.js
+++ a02/sketch.js
@@ -1,14 +1,16 @@
 const cWidth = 1280; // canvas width
 const cHeight = 700; // canvas height
+const fontSize = 16; // framerate font size
+let showFrameRate = false; // draw framerate on canvas (or stop drawing)
+console.log('Display the framerate by setting "showFrameRate: true" in the console.');
+
 const inc = 10; // how often the noise position should update ~[1 - 15]
 const strength = 0.1; // "gravitational" force of the vectors on dots ~[0.1 - 10]
 const chaosFactor = 2; // the higher, the more inconsistent the vector angles get ~[0.1 - 4]
-const particleAmount = 60; // amounts of particles on the canvas
-const particleMaxSpeed = 0.5; // maximum particle travel speed ~[1- 10]
+const particleAmount = 100; // amounts of particles on the canvas
+const particleMaxSpeed = 1; // maximum particle travel speed ~[1- 10]
 const particleThickness = 40; // line thickness of particles ~[1 - 20]
-const particleDownforce = 1; // downforce on particles aka gravitation
-const fontSize = 16; // framerate font size
-let showFrameRate = false; // draw framerate on canvas (or stop drawing)
+const particleDownforce = 1; // downforce on particles aka gravitation ~[0.1 - 5]
 
 let zOff = 0;
 let particles = new Array(particleAmount).fill(null);
