4366297aae1bbc51e369893f5af5a56ab4e66c88
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-03 16:46:59 +0100

    a02: add rotation to particle images

--- a02/particle.js
+++ a02/particle.js
@@ -1,14 +1,15 @@
 // eslint-disable-next-line no-unused-vars
-function Particle(image, maxSpeed, thickness, downforce) {
+function Particle(image, maxSpeed, size, downforce) {
+  this.size = size;
   this.image = image;
   this.pos = createVector(random(width), random(height));
   this.velo = createVector(0, 0);
   this.accel = createVector(0, 0);
-  this.gravitation = p5.Vector.fromAngle(90 * (PI / 180)).setMag(downforce / 10);
+  this.gravitationVector = p5.Vector.fromAngle(random(90, 110) * (PI / 180)).setMag(downforce / 10);
+  this.rotationAngle = floor(random(0, 180));
+  this.hasInversRotation = random(0, 1) > 0.5;
   this.maxSpeed = maxSpeed;
   this.prevPos = this.pos.copy();
-  this.hue = 0;
-  this.strokeWeight = thickness;
 
   this.update = function () {
     this.velo.add(this.accel);
@@ -17,34 +18,32 @@ function Particle(image, maxSpeed, thickness, downforce) {
     this.accel.mult(0); // reset acceleration to 0
   };
 
-  this.applyForce = function (vector) {
-    this.accel.add(vector);
-    this.accel.add(this.gravitation);
-  };
-
-  this.follow = function (zOff, strength, chaosFactor) {
-    let angle = noise(this.pos.x, this.pos.y, zOff) * TWO_PI * chaosFactor;
-    const vector = p5.Vector.fromAngle(angle);
-    vector.setMag(strength);
-    this.applyForce(vector);
-  };
-
-  this.show = function (graphic) {
-    graphic.imageMode(CORNER);
-    graphic.image(
-      this.image,
-      this.pos.x,
-      this.pos.y,
-      this.strokeWeight,
-      (this.image.height * this.strokeWeight) / this.image.width
-    );
-
-    this.updatePrev();
-  };
-
   this.updatePrev = function () {
     this.prevPos.x = this.pos.x;
     this.prevPos.y = this.pos.y;
+  };
+
+  this.applyForce = function (extraForceVector) {
+    this.accel.add(this.gravitationVector);
+    this.accel.add(extraForceVector);
+  };
+
+  this.follow = function (zOff, strength, chaosFactor) {
+    const angle = noise(this.pos.x, this.pos.y, zOff) * TWO_PI * chaosFactor;
+    const forceVector = p5.Vector.fromAngle(angle).setMag(strength);
+    this.applyForce(forceVector);
+  };
+
+  this.show = function (graphic) {
+    graphic.push();
+    graphic.translate(this.pos.x, this.pos.y);
+    graphic.rotate(this.rotationAngle * (PI / 180));
+    this.rotationAngle += random(0, this.hasInversRotation ? -1 : 1);
+    graphic.imageMode(CENTER);
+    graphic.image(this.image, 0, 0, this.size, (this.image.height * this.size) / this.image.width);
+    graphic.pop();
+
+    this.updatePrev();
   };
 
   this.edges = function () {
