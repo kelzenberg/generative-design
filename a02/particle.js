3295ab828c4cb5f8d302c26081760b743686f19c
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-03 17:11:02 +0100

    a02: fix performance bug with graphics & cleanup

--- a02/particle.js
+++ a02/particle.js
@@ -1,42 +1,42 @@
 // eslint-disable-next-line no-unused-vars
-function Particle(image, maxSpeed, size, downforce) {
+function Particle(image, maxVelocity, size, downforce) {
   this.size = size;
   this.image = image;
-  this.pos = createVector(random(width), random(height));
-  this.velo = createVector(0, 0);
-  this.accel = createVector(0, 0);
+  this.position = createVector(random(width), random(height));
+  this.prevPosition = this.position.copy();
+  this.velocity = createVector(0, 0);
+  this.acceleration = createVector(0, 0);
   this.gravitationVector = p5.Vector.fromAngle(random(90, 110) * (PI / 180)).setMag(downforce / 10);
   this.rotationAngle = floor(random(0, 180));
   this.hasInversRotation = random(0, 1) > 0.5;
-  this.maxSpeed = maxSpeed;
-  this.prevPos = this.pos.copy();
+  this.maxVelocity = maxVelocity;
 
   this.update = function () {
-    this.velo.add(this.accel);
-    this.velo.limit(this.maxSpeed);
-    this.pos.add(this.velo);
-    this.accel.mult(0); // reset acceleration to 0
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxVelocity);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0); // reset acceleration to 0
   };
 
   this.updatePrev = function () {
-    this.prevPos.x = this.pos.x;
-    this.prevPos.y = this.pos.y;
+    this.prevPosition.x = this.position.x;
+    this.prevPosition.y = this.position.y;
   };
 
-  this.applyForce = function (extraForceVector) {
-    this.accel.add(this.gravitationVector);
-    this.accel.add(extraForceVector);
+  this.applyForce = function (forceVector) {
+    this.acceleration.add(this.gravitationVector);
+    this.acceleration.add(forceVector);
   };
 
   this.follow = function (zOff, strength, chaosFactor) {
-    const angle = noise(this.pos.x, this.pos.y, zOff) * TWO_PI * chaosFactor;
+    const angle = noise(this.position.x, this.position.y, zOff) * TWO_PI * chaosFactor;
     const forceVector = p5.Vector.fromAngle(angle).setMag(strength);
     this.applyForce(forceVector);
   };
 
   this.show = function (graphic) {
     graphic.push();
-    graphic.translate(this.pos.x, this.pos.y);
+    graphic.translate(this.position.x, this.position.y);
     graphic.rotate(this.rotationAngle * (PI / 180));
     this.rotationAngle += random(0, this.hasInversRotation ? -1 : 1);
     graphic.imageMode(CENTER);
@@ -47,20 +47,20 @@ function Particle(image, maxSpeed, size, downforce) {
   };
 
   this.edges = function () {
-    if (this.pos.x > width) {
-      this.pos.x = 0;
+    if (this.position.x > width) {
+      this.position.x = 0;
       this.updatePrev();
     }
-    if (this.pos.x < 0) {
-      this.pos.x = width;
+    if (this.position.x < 0) {
+      this.position.x = width;
       this.updatePrev();
     }
-    if (this.pos.y > height) {
-      this.pos.y = 0;
+    if (this.position.y > height) {
+      this.position.y = 0;
       this.updatePrev();
     }
-    if (this.pos.y < 0) {
-      this.pos.y = height;
+    if (this.position.y < 0) {
+      this.position.y = height;
       this.updatePrev();
     }
   };
--- a02/sketch.js
+++ a02/sketch.js
@@ -5,9 +5,9 @@ let showFrameRate = false; // draw framerate on canvas (or stop drawing)
 console.log('Display the framerate by setting "showFrameRate: true" in the console.');
 
 const inc = 10; // how often the noise position should update ~[1 - 15]
-const strength = 0.1; // "gravitational" force of the vectors on dots ~[0.1 - 10]
+const strength = 0.2; // "gravitational" force of the vectors on dots ~[0.1 - 10]
 const chaosFactor = 2; // the higher, the more inconsistent the vector angles get ~[0.1 - 4]
-const particleAmount = 100; // amounts of particles on the canvas
+const particleAmount = 100; // amounts of particles on the canvas ~[100]
 const particleMaxSpeed = 1; // maximum particle travel speed ~[1- 10]
 const particleThickness = 40; // line thickness of particles ~[1 - 20]
 const particleDownforce = 1; // downforce on particles aka gravitation ~[0.1 - 5]
@@ -28,7 +28,6 @@ function preload() {
 // eslint-disable-next-line no-unused-vars
 function setup() {
   createCanvas(cWidth, cHeight);
-  // createCanvas(windowWidth, windowHeight);
   console.log(`Canvas: ${width} x ${height}`);
   pixelDensity(1);
   noiseDetail(64);
@@ -48,14 +47,11 @@ function drawFrameRate() {
   image(graphic, 0, 0);
 }
 
-// eslint-disable-next-line no-unused-vars
-function draw() {
-  background(255);
-  image(forestImg, 0, 0, width, (forestImg.height * width) / forestImg.width);
+function drawParticles() {
+  zOff += inc / 1000.0;
 
   const particleGraphic = createGraphics(cWidth, cHeight);
   particleGraphic.colorMode(HSB, 255);
-  zOff += inc / 1000.0;
 
   particles.map(particle => {
     particle.follow(zOff, strength, chaosFactor);
@@ -64,7 +60,13 @@ function draw() {
     particle.show(particleGraphic);
   });
 
-  image(particleGraphic, 0, 0);
+  return particleGraphic;
+}
+
+// eslint-disable-next-line no-unused-vars
+function draw() {
+  image(forestImg, 0, 0, width, (forestImg.height * width) / forestImg.width);
+  image(drawParticles(), 0, 0);
   image(roomImg, 0, 0, width, (roomImg.height * width) / roomImg.width);
 
   if (showFrameRate) {
