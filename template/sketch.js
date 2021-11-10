774b007aa03d6a861ef91eb09b70e0de68cd5b62
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-10 18:45:49 +0100

    update template

--- template/sketch.js
+++ template/sketch.js
@@ -1,12 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 function setup() {
   createCanvas(500, 500);
   noLoop();
 }
 
+// eslint-disable-next-line no-unused-vars
 function draw() {
-  background("blue");
+  background('blue');
 }
 
+// eslint-disable-next-line no-unused-vars
 function windowResized() {
   resizeCanvas(500, 500);
 }
