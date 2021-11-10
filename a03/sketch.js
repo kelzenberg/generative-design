4f41bbe6f0134061880b95cd7c32afc7835d97d8
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-10 19:24:52 +0100

    a03: init assignment

---
+++ a03/sketch.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+function setup() {
+  createCanvas(500, 500);
+  noLoop();
+}
+
+// eslint-disable-next-line no-unused-vars
+function draw() {
+  background('blue');
+}
+
+// eslint-disable-next-line no-unused-vars
+function windowResized() {
+  resizeCanvas(500, 500);
+}
