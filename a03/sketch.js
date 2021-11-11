4e1160e74e2a118a67c82d14bc25dec5e74e5a1e
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-11 18:33:21 +0100

    a03: add vehicle & behaviors

--- a03/sketch.js
+++ a03/sketch.js
@@ -10,19 +10,31 @@ const wind = 5;
 const dragCoefficient = 1;
 const gravitationalC = 50; // ~[1 - 50]
 
+let font;
+let points;
+let vehicles;
+
+// eslint-disable-next-line no-unused-vars
+function preload() {
+  font = loadFont('./font/Alice-Regular.ttf');
+}
+
 // eslint-disable-next-line no-unused-vars
 function setup() {
   createCanvas(cWidth, cHeight);
   createFrameRate(cWidth, cHeight);
 
-  attractors.push(new Attractor(width / 2 - 100, height / 2, 1));
-  attractors.push(new Attractor(width / 2 + 100, height / 2, 3));
-  movers = movers.map(() => new Mover(random(50, cWidth), random(50, cHeight), random(10, 100)));
+  // attractors.push(new Attractor(width / 2 - 100, height / 2, 1));
+  // attractors.push(new Attractor(width / 2 + 100, height / 2, 3));
+  // movers = movers.map(() => new Mover(random(50, cWidth), random(50, cHeight), random(10, 100)));
+
+  points = font.textToPoints('Code runs!', 50, height / 2, 100);
+  vehicles = points.map(pt => new Vehicle(pt.x, pt.y));
 }
 
 // eslint-disable-next-line no-unused-vars
 function draw() {
-  background(0, 10);
+  background(0);
   drawFrameRate();
 
   // const liquidStart = height - height * 0.2;
@@ -30,26 +42,32 @@ function draw() {
   // noStroke();
   // rect(0, liquidStart, width, height * 0.2);
 
-  for (const mover of movers) {
-    // let relGravity = p5.Vector.mult(createVector(0, gravity), mover.mass);
-    // mover.applyForce(relGravity);
+  // for (const mover of movers) {
+  //   // let relGravity = p5.Vector.mult(createVector(0, gravity), mover.mass);
+  //   // mover.applyForce(relGravity);
 
-    if (mouseIsPressed) {
-      mover.applyForce(createVector(wind, 0));
-    }
+  //   if (mouseIsPressed) {
+  //     mover.applyForce(createVector(wind, 0));
+  //   }
 
-    // if (mover.position.y > liquidStart) {
-    //   mover.drag(dragCoefficient);
-    // }
+  //   // if (mover.position.y > liquidStart) {
+  //   //   mover.drag(dragCoefficient);
+  //   // }
 
-    mover.friction(mu);
-    mover.update();
-    mover.edges();
-    mover.show();
+  //   mover.friction(mu);
+  //   mover.update();
+  //   mover.edges();
+  //   mover.show();
 
-    for (const attractor of attractors) {
-      attractor.attract(gravitationalC, mover);
-      attractor.show();
-    }
+  //   for (const attractor of attractors) {
+  //     attractor.attract(gravitationalC, mover);
+  //     attractor.show();
+  //   }
+  // }
+
+  for (const vehicle of vehicles) {
+    vehicle.applyBehaviors();
+    vehicle.update();
+    vehicle.show();
   }
 }
---
+++ a03/vehicle.js
@@ -0,0 +1,68 @@
+// eslint-disable-next-line no-unused-vars
+class Vehicle {
+  constructor(x, y) {
+    this.position = createVector(random(width), random(height));
+    this.target = createVector(x, y);
+    this.velocity = p5.Vector.random2D();
+    this.acceleration = createVector(0, 0);
+    this.radius = 4;
+    this.maxSpeed = 8;
+    this.maxForce = 1;
+  }
+
+  applyForce(force) {
+    this.acceleration.add(force);
+  }
+
+  steerWith(desired) {
+    const steer = p5.Vector.sub(desired, this.velocity);
+    steer.limit(this.maxForce);
+    return steer;
+  }
+
+  fleeFrom(target) {
+    const desiredVelocity = p5.Vector.sub(target, this.position); // Vector pointing from position to target
+    const distanceToTarget = desiredVelocity.mag();
+
+    if (distanceToTarget > 50) {
+      return createVector(0, 0);
+    }
+
+    desiredVelocity.setMag(this.maxSpeed);
+    desiredVelocity.mult(-1);
+
+    return this.steerWith(desiredVelocity);
+  }
+
+  striveTo() {
+    const desiredVelocity = p5.Vector.sub(this.target, this.position); // Vector pointing from position to target
+    const distanceToTarget = desiredVelocity.mag();
+
+    const speed = distanceToTarget > 100 ? this.maxSpeed : map(distanceToTarget, 0, 100, 0, this.maxSpeed);
+    desiredVelocity.setMag(speed);
+
+    return this.steerWith(desiredVelocity);
+  }
+
+  applyBehaviors() {
+    const arrive = this.striveTo(this.target);
+    const flee = this.fleeFrom(createVector(mouseX, mouseY));
+    arrive.mult(1);
+    flee.mult(5);
+
+    this.applyForce(arrive);
+    this.applyForce(flee);
+  }
+
+  update() {
+    this.position.add(this.velocity);
+    this.velocity.add(this.acceleration);
+    this.acceleration.mult(0);
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(this.radius);
+    point(this.position.x, this.position.y);
+  }
+}
