290c33a3373bd5858b61a227e13d1a0bafc8eb66
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-28 15:47:37 +0100

    a04: add angular motion

--- a04/sketch.js
+++ a04/sketch.js
@@ -7,9 +7,27 @@ function setup() {
   createFrameRate(cWidth, cHeight);
 }
 
+let angle = 0;
+let angleVelocity = 0.05;
+let angleAcceleration = 0.001;
+
 // eslint-disable-next-line no-unused-vars
 function draw() {
   background('blue');
+
+  angleAcceleration = map(mouseX, 0, width, -0.01, 0.01);
+  angleVelocity = constrain(angleVelocity, -0.2, 0.2);
+
+  noStroke();
+  fill(255, 255, 0);
+  rectMode(CENTER);
+  translate(cWidth / 2, cHeight / 2);
+  rotate(angle);
+  rect(0, 0, 200, 50);
+
+  angle += angleVelocity;
+  angleVelocity += angleAcceleration;
+
   drawFrameRate();
 }
 
