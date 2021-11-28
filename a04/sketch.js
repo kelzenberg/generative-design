8790bd8ce063f024920e0a87e45a5939ddcea60d
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-28 16:26:48 +0100

    a04: draw manual shape (circle) by radius

--- a04/sketch.js
+++ a04/sketch.js
@@ -15,25 +15,38 @@ function setup() {
 
 // eslint-disable-next-line no-unused-vars
 function draw() {
-  background(0, 0, 0, 2);
+  background(0, 0, 0, 50);
 
   // angleAcceleration = map(mouseX, 0, width, -0.01, 0.01);
   // angleVelocity = constrain(angleVelocity, -0.2, 0.2);
 
   translate(cWidth / 2, cHeight / 2);
-  stroke('white');
-  // noFill();
-  // strokeWeight(4);
+
+  strokeWeight(4);
+  stroke(255);
+  noFill();
   // circle(0, 0, radius * 2);
+
+  // let increment = map(mouseX, 0, width, PI, 0.01);
+  let increment = 0.1;
+
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += increment) {
+    let r1 = radius + random(-10, 10);
+    let x = cos(a) * r1;
+    let y = sin(a) * r1;
+    vertex(x, y);
+  }
+  endShape(CLOSE); // to close circle line
 
   strokeWeight(32);
   stroke(252, 238, 33);
   let x = cos(angle) * radius;
   let y = sin(angle) * radius;
-  point(x, y);
+  // point(x, y);
 
   angle += 0.05;
-  radius += random(-2, 2);
+  // radius += random(-2, 2);
 
   drawFrameRate();
 }
