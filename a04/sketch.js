27391300cc7634d7fd92b9dcff6b39c1305b27b3
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-28 16:17:29 +0100

    a04: move by polar coordinates

--- a04/sketch.js
+++ a04/sketch.js
@@ -1,32 +1,39 @@
 const cWidth = 500; // canvas width
 const cHeight = 500; // canvas height
 
-// eslint-disable-next-line no-unused-vars
-function setup() {
-  createCanvas(cWidth, cHeight);
-  createFrameRate(cWidth, cHeight);
-}
-
+let radius = 150;
 let angle = 0;
 let angleVelocity = 0.05;
 let angleAcceleration = 0.001;
 
 // eslint-disable-next-line no-unused-vars
+function setup() {
+  createCanvas(cWidth, cHeight);
+  createFrameRate(cWidth, cHeight);
+  background(0);
+}
+
+// eslint-disable-next-line no-unused-vars
 function draw() {
-  background('blue');
+  background(0, 0, 0, 2);
 
-  angleAcceleration = map(mouseX, 0, width, -0.01, 0.01);
-  angleVelocity = constrain(angleVelocity, -0.2, 0.2);
+  // angleAcceleration = map(mouseX, 0, width, -0.01, 0.01);
+  // angleVelocity = constrain(angleVelocity, -0.2, 0.2);
 
-  noStroke();
-  fill(255, 255, 0);
-  rectMode(CENTER);
   translate(cWidth / 2, cHeight / 2);
-  rotate(angle);
-  rect(0, 0, 200, 50);
+  stroke('white');
+  // noFill();
+  // strokeWeight(4);
+  // circle(0, 0, radius * 2);
 
-  angle += angleVelocity;
-  angleVelocity += angleAcceleration;
+  strokeWeight(32);
+  stroke(252, 238, 33);
+  let x = cos(angle) * radius;
+  let y = sin(angle) * radius;
+  point(x, y);
+
+  angle += 0.05;
+  radius += random(-2, 2);
 
   drawFrameRate();
 }
