63328d244d242158e9c6d3ce445c86ef8e26c7b8
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-28 18:46:21 +0100

    a04: finish clock

--- a04/sketch.js
+++ a04/sketch.js
@@ -39,31 +39,50 @@ function draw() {
   //   wave.updatePhase(0.05);
   // }
 
-  let ho = hour();
-  let mi = minute();
-  let se = second();
+  let hou = hour();
+  let min = minute();
+  let sec = second();
   let mil = millis();
 
   fill('white');
   noStroke();
-  text(`${ho}:${mi}:${se}`, cWidth / 2 - 20, cHeight / 2); // debug
+  text(`${hou}:${min}:${sec}`, cWidth / 2 - 20, 100); // debug
 
   translate(cWidth / 2, cHeight / 2);
   rotate(-PI / 2);
 
-  stroke(255, 100, 150);
   strokeWeight(12);
   noFill();
-  let end1 = map(mil, 0, 60 * 1000, 0, TWO_PI);
-  arc(0, 0, cWidth / 2, cHeight / 2, 0, end1);
+
+  stroke(255, 100, 150, 255);
+  let milAngle = map(mil, 0, 60 * 1000, 0, TWO_PI);
+  arc(0, 0, cWidth / 2, cHeight / 2, 0, milAngle);
+
+  push();
+  rotate(milAngle);
+  stroke(255, 100, 150, 255);
+  line(0, 0, 80, 0);
+  pop();
 
   stroke(255, 100, 150, 150);
-  let end2 = map(mi, 0, 60, 0, TWO_PI);
-  arc(0, 0, cWidth / 2 - 20, cHeight / 2 - 20, 0, end2);
+  let minAngle = map(min, 0, 60, 0, TWO_PI);
+  arc(0, 0, cWidth / 2 - 20, cHeight / 2 - 20, 0, minAngle);
+
+  push();
+  rotate(minAngle);
+  stroke(255, 100, 150, 150);
+  line(0, 0, 60, 0);
+  pop();
 
   stroke(255, 100, 150, 50);
-  let end3 = map(ho % 12, 0, 12, 0, TWO_PI);
-  arc(0, 0, cWidth / 2 - 40, cHeight / 2 - 40, 0, end3);
+  let houAngle = map(hou % 12, 0, 12, 0, TWO_PI);
+  arc(0, 0, cWidth / 2 - 40, cHeight / 2 - 40, 0, houAngle);
+
+  push();
+  rotate(houAngle);
+  stroke(255, 100, 150, 50);
+  line(0, 0, 40, 0);
+  pop();
 }
 
 // eslint-disable-next-line no-unused-vars
