05fa0898eaf75fe247581171b12a6ac285454319
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-28 17:45:20 +0100

    a04: use sine wave for oscillation

--- a04/sketch.js
+++ a04/sketch.js
@@ -1,7 +1,7 @@
 const cWidth = 500; // canvas width
 const cHeight = 500; // canvas height
 
-let radius = 16;
+let radius = 4;
 let angles = [];
 let angleVelocities = [];
 
@@ -11,9 +11,10 @@ function setup() {
   createFrameRate(cWidth, cHeight);
   background(0);
 
+  let frequency = TWO_PI * 2;
   let total = floor(width / (radius * 2));
-  for (let idx = 0; idx < total; idx++) {
-    angles[idx] = 0;
+  for (let idx = 0; idx < total + 1; idx++) {
+    angles[idx] = map(idx, 0, total, 0, frequency);
     // angleVelocities[idx] = random(-0.1, 0.1);
     angleVelocities[idx] = idx / 100;
   }
@@ -26,19 +27,25 @@ function draw() {
 
   translate(cWidth / 2, cHeight / 2);
 
+  beginShape();
   for (let idx = 0; idx < angles.length; idx += 1) {
     let y = map(sin(angles[idx]), -1, 1, -cHeight / 2 + radius, cHeight / 2 - radius);
     let x = map(idx, 0, angles.length, -cWidth / 2 + radius, cWidth / 2 + radius);
 
-    line(x, 0, x, y);
+    // line(x, 0, x, y);
 
-    stroke('white');
-    fill(252, 238, 33);
-    circle(x, y, 32);
+    fill('yellow');
+    circle(x, y, radius * 2);
 
-    angles[idx] += angleVelocities[idx];
-    angleVelocities[idx] += 0.00001;
+    // noFill();
+    // stroke('yellow');
+    // strokeWeight(4);
+    // vertex(x, y);
+
+    angles[idx] += 0.02;
+    // angles[idx] += angleVelocities[idx];
   }
+  endShape();
 }
 
 // eslint-disable-next-line no-unused-vars
