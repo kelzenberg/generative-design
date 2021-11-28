3c5f9bf5ce0473957f7d1630cb3d8d5a1099f62a
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-28 17:34:34 +0100

    a04: oscillate dots in wave pattern

--- a04/sketch.js
+++ a04/sketch.js
@@ -1,15 +1,22 @@
 const cWidth = 500; // canvas width
 const cHeight = 500; // canvas height
 
-let radius = 150;
-let angle = 0;
-let angleVelocity = 0;
+let radius = 16;
+let angles = [];
+let angleVelocities = [];
 
 // eslint-disable-next-line no-unused-vars
 function setup() {
   createCanvas(cWidth, cHeight);
   createFrameRate(cWidth, cHeight);
   background(0);
+
+  let total = floor(width / (radius * 2));
+  for (let idx = 0; idx < total; idx++) {
+    angles[idx] = 0;
+    // angleVelocities[idx] = random(-0.1, 0.1);
+    angleVelocities[idx] = idx / 100;
+  }
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -19,18 +26,19 @@ function draw() {
 
   translate(cWidth / 2, cHeight / 2);
 
-  radius = map(sin(angle), -1, 1, 0, cWidth / 2); // cWidth/2 is sine-amplitude / 2
-  let y = map(sin(angle), -1, 1, -cWidth / 2, cWidth / 2);
-  line(0, 0, 0, y);
-  stroke('white');
-  fill(252, 238, 33);
-  circle(0, y, 32);
+  for (let idx = 0; idx < angles.length; idx += 1) {
+    let y = map(sin(angles[idx]), -1, 1, -cHeight / 2 + radius, cHeight / 2 - radius);
+    let x = map(idx, 0, angles.length, -cWidth / 2 + radius, cWidth / 2 + radius);
 
-  // let increment = TWO_PI / 60; // aka 60 fps
-  // angle += increment;
-  angle += angleVelocity;
+    line(x, 0, x, y);
 
-  angleVelocity += 0.0001;
+    stroke('white');
+    fill(252, 238, 33);
+    circle(x, y, 32);
+
+    angles[idx] += angleVelocities[idx];
+    angleVelocities[idx] += 0.00001;
+  }
 }
 
 // eslint-disable-next-line no-unused-vars
