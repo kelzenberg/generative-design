71bc6d5a843832372176519b4fca644fc30e0764
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-28 18:39:06 +0100

    a04: rotate clock

--- a04/sketch.js
+++ a04/sketch.js
@@ -42,24 +42,28 @@ function draw() {
   let ho = hour();
   let mi = minute();
   let se = second();
+  let mil = millis();
+
+  fill('white');
+  noStroke();
+  text(`${ho}:${mi}:${se}`, cWidth / 2 - 20, cHeight / 2); // debug
+
+  translate(cWidth / 2, cHeight / 2);
+  rotate(-PI / 2);
 
   stroke(255, 100, 150);
   strokeWeight(12);
   noFill();
-  let end1 = map(se, 0, 60, 0, TWO_PI);
-  arc(cWidth / 2, cHeight / 2, cWidth / 2, cHeight / 2, 0, end1);
+  let end1 = map(mil, 0, 60 * 1000, 0, TWO_PI);
+  arc(0, 0, cWidth / 2, cHeight / 2, 0, end1);
 
   stroke(255, 100, 150, 150);
   let end2 = map(mi, 0, 60, 0, TWO_PI);
-  arc(cWidth / 2, cHeight / 2, cWidth / 2 - 20, cHeight / 2 - 20, 0, end2);
+  arc(0, 0, cWidth / 2 - 20, cHeight / 2 - 20, 0, end2);
 
   stroke(255, 100, 150, 50);
-  let end3 = map(ho, 0, 24, 0, TWO_PI);
-  arc(cWidth / 2, cHeight / 2, cWidth / 2 - 40, cHeight / 2 - 40, 0, end3);
-
-  // fill('white');
-  // noStroke();
-  // text(`${ho}:${mi}:${se}`, 10, cHeight / 2);
+  let end3 = map(ho % 12, 0, 12, 0, TWO_PI);
+  arc(0, 0, cWidth / 2 - 40, cHeight / 2 - 40, 0, end3);
 }
 
 // eslint-disable-next-line no-unused-vars
