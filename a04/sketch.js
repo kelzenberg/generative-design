4c248b6c200d34d8a7637c92bc4a6f1e6daf0278
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-11-28 16:49:09 +0100

    a04: add harmonic motion through sine

--- a04/sketch.js
+++ a04/sketch.js
@@ -3,8 +3,7 @@ const cHeight = 500; // canvas height
 
 let radius = 150;
 let angle = 0;
-let angleVelocity = 0.05;
-let angleAcceleration = 0.001;
+let angleVelocity = 0;
 
 // eslint-disable-next-line no-unused-vars
 function setup() {
@@ -15,40 +14,23 @@ function setup() {
 
 // eslint-disable-next-line no-unused-vars
 function draw() {
-  background(0, 0, 0, 50);
-
-  // angleAcceleration = map(mouseX, 0, width, -0.01, 0.01);
-  // angleVelocity = constrain(angleVelocity, -0.2, 0.2);
+  background(0, 0, 0);
+  drawFrameRate();
 
   translate(cWidth / 2, cHeight / 2);
 
-  strokeWeight(4);
-  stroke(255);
-  noFill();
-  // circle(0, 0, radius * 2);
+  radius = map(sin(angle), -1, 1, 0, cWidth / 2); // cWidth/2 is sine-amplitude / 2
+  let y = map(sin(angle), -1, 1, -cWidth / 2, cWidth / 2);
+  line(0, 0, 0, y);
+  stroke('white');
+  fill(252, 238, 33);
+  circle(0, y, 32);
 
-  // let increment = map(mouseX, 0, width, PI, 0.01);
-  let increment = 0.1;
+  // let increment = TWO_PI / 60; // aka 60 fps
+  // angle += increment;
+  angle += angleVelocity;
 
-  beginShape();
-  for (let a = 0; a < TWO_PI; a += increment) {
-    let r1 = radius * noise(a) * 2;
-    let x = cos(a) * r1;
-    let y = sin(a) * r1;
-    vertex(x, y);
-  }
-  endShape(CLOSE); // to close circle line
-
-  strokeWeight(32);
-  stroke(252, 238, 33);
-  let x = cos(angle) * radius;
-  let y = sin(angle) * radius;
-  // point(x, y);
-
-  angle += 0.05;
-  // radius += random(-2, 2);
-
-  drawFrameRate();
+  angleVelocity += 0.0001;
 }
 
 // eslint-disable-next-line no-unused-vars
