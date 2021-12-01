5e6c2c786d7178f6eba3eeaa101ad1a8984ec62d
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-12-01 17:16:25 +0100

    a04: add dummy button & background color change

--- a04/sketch.js
+++ a04/sketch.js
@@ -11,6 +11,10 @@ function setup() {
   createFrameRate(cWidth, cHeight);
   background(20);
 
+  button = createButton('click me');
+  button.position(0, 0);
+  button.mousePressed(() => console.log('foo'));
+
   oceanLayer = [
     // hour
     new OceanLayer({
@@ -56,55 +60,17 @@ function setup() {
 
 // eslint-disable-next-line no-unused-vars
 function draw() {
-  background(20, 255);
+  if (hour() >= 17) {
+    background(0, 10, 35);
+  } else {
+    background(180, 225, 255);
+  }
+
   drawFrameRate();
 
   for (const layer of oceanLayer) {
     layer.draw();
   }
-
-  let hou = hour();
-  let min = minute();
-  let sec = second();
-  // let mil = millis();
-  let scaling = Math.min(cWidth, cHeight);
-
-  fill('white');
-  noStroke();
-  text(`${hou}:${min}:${sec}`, cWidth / 2 - 20, 100); // debug
-
-  // translate(cWidth / 2, cHeight / 2);
-  // rotate(-PI / 2);
-
-  // strokeWeight(scaling / (12 * 5));
-  // noFill();
-
-  // // seconds
-  // stroke(255, 85);
-  // let milAngle = map(sec, 0, 60, 0, TWO_PI);
-  // arc(0, 0, cWidth / cHeight + scaling * 0.6, cHeight / cWidth + scaling * 0.6, 0, milAngle);
-  // push();
-  // rotate(milAngle);
-  // line(0, 0, map(80, 0, 80, 0, scaling / 5), 0);
-  // pop();
-
-  // // minutes
-  // stroke(255, 170);
-  // let minAngle = map(min, 0, 60, 0, TWO_PI);
-  // arc(0, 0, cWidth / cHeight + scaling * 0.6 - 20, cHeight / cWidth + scaling * 0.6 - 20, 0, minAngle);
-  // push();
-  // rotate(minAngle);
-  // line(0, 0, map(60, 0, 80, 0, scaling / 5), 0);
-  // pop();
-
-  // // hours
-  // stroke(255, 255);
-  // let houAngle = map(hou % 12, 0, 12, 0, TWO_PI);
-  // arc(0, 0, cWidth / cHeight + scaling * 0.6 - 40, cHeight / cWidth + scaling * 0.6 - 40, 0, houAngle);
-  // push();
-  // rotate(houAngle);
-  // line(0, 0, map(40, 0, 80, 0, scaling / 5), 0);
-  // pop();
 }
 
 // eslint-disable-next-line no-unused-vars
