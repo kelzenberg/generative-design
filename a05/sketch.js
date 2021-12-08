82250cea47686ebd4bdba79d33ab09f1b248953f
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-12-08 14:48:13 +0100

    a05: init assignment

---
+++ a05/sketch.js
@@ -0,0 +1,19 @@
+const cWidth = 500; // canvas width
+const cHeight = 500; // canvas height
+
+// eslint-disable-next-line no-unused-vars
+function setup() {
+  createCanvas(cWidth, cHeight);
+  createFrameRate(cWidth, cHeight);
+}
+
+// eslint-disable-next-line no-unused-vars
+function draw() {
+  background('blue');
+  drawFrameRate();
+}
+
+// eslint-disable-next-line no-unused-vars
+// function windowResized() {
+//   resizeCanvas(cWidth, cHeight);
+// }
