ac7aebfd9134b9e9f99a6d194851f0da9f4ad0f2
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-12-09 14:02:48 +0100

    a05: set emitters to mouse position on click

--- a05/sketch.js
+++ a05/sketch.js
@@ -1,15 +1,17 @@
 const cWidth = 350; // canvas width
 const cHeight = 350; // canvas height
 
-// let particles = [];
-let emitter;
+let emitters = [];
 
 // eslint-disable-next-line no-unused-vars
 function setup() {
   createCanvas(cWidth, cHeight);
   createFrameRate(cWidth, cHeight);
+}
 
-  emitter = new Emitter(cWidth / 2, 20);
+// eslint-disable-next-line no-unused-vars
+function mousePressed() {
+  emitters.push(new Emitter(mouseX, mouseY));
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -17,27 +19,11 @@ function draw() {
   background(0);
   drawFrameRate();
 
-  emitter.emit(2);
-  emitter.update();
-  emitter.show();
-
-  // for (let idx = 0; idx < 1; idx++) {
-  //   particles.push(new Particle(createVector(cWidth / 2, 20)));
-  // }
-
-  // const gravity = createVector(0, 0.2);
-  // for (const particle of particles) {
-  //   particle.applyForce(gravity);
-  //   particle.update();
-  //   particle.avoidEdges();
-  //   particle.show();
-  // }
-
-  // for (let idx = particles.length - 1; idx >= 0; idx--) {
-  //   if (particles[idx].isFinished()) {
-  //     particles.splice(idx, 1);
-  //   }
-  // }
+  for (const emitter of emitters) {
+    emitter.emit(2);
+    emitter.update();
+    emitter.show();
+  }
 }
 
 // eslint-disable-next-line no-unused-vars
