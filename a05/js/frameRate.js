bd73f5680d8217c4acc3affc920ea1adc02eb080
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2021-12-12 21:56:08 +0100

    a05: finish WebGl conversion

--- a05/js/frameRate.js
+++ a05/js/frameRate.js
@@ -1,31 +1,41 @@
 const frameRateConfig = {
-  graphic: null, // graphic to draw framerate on
+  graphic: null, // texture for plane
   fontSize: 16, // framerate font size
+  cWidth: null,
+  cHeight: null,
 };
 
-let showFrameRate = false; // draw framerate on graphic (or stop drawing)
+let showFrameRate = false; // draw framerate
 console.log('Display the framerate by setting "showFrameRate = true" in the console.');
 
 // eslint-disable-next-line no-unused-vars
 function createFrameRate(cWidth, cHeight) {
-  frameRateConfig.graphic = createGraphics(cWidth, cHeight);
-  frameRateConfig.graphic.textSize(frameRateConfig.fontSize);
-  frameRateConfig.graphic.noStroke();
+  const { fontSize } = frameRateConfig;
+  const graphic = createGraphics(fontSize + fontSize / 8, fontSize + fontSize / 8);
+  graphic.textSize(fontSize);
+  graphic.noStroke();
+
+  frameRateConfig.cWidth = cWidth;
+  frameRateConfig.cHeight = cHeight;
+  frameRateConfig.graphic = graphic;
 }
 
 // eslint-disable-next-line no-unused-vars
 function drawFrameRate() {
-  frameRateConfig.graphic.fill('white');
-  frameRateConfig.graphic.rect(
-    0,
-    0,
-    frameRateConfig.fontSize + frameRateConfig.fontSize / 8,
-    frameRateConfig.fontSize + frameRateConfig.fontSize / 8
-  );
-  frameRateConfig.graphic.fill('black');
-  frameRateConfig.graphic.text(frameRate().toFixed(), 0, frameRateConfig.fontSize);
+  if (!showFrameRate) return;
 
-  if (showFrameRate) {
-    image(frameRateConfig.graphic, 0, 0);
-  }
+  const { graphic, fontSize, cWidth, cHeight } = frameRateConfig;
+
+  graphic.fill('white');
+  graphic.rect(0, 0, fontSize + fontSize / 8, fontSize + fontSize / 8);
+  graphic.fill('black');
+  graphic.text(frameRate().toFixed(), 0, fontSize);
+
+  push();
+  translate(-cWidth / 2 + fontSize, -cHeight / 2 + fontSize);
+
+  noStroke();
+  texture(graphic);
+  plane(graphic.width, graphic.height);
+  pop();
 }
--- a05/js/sketch.js
+++ a05/js/sketch.js
@@ -3,6 +3,8 @@ let cHeight = 500; // canvas height
 
 const emitters = [];
 let particleImage;
+let angle = 0;
+let mouseZ = 0;
 
 // eslint-disable-next-line no-unused-vars
 function preload() {
@@ -23,11 +25,31 @@ function mousePressed() {
 }
 
 // eslint-disable-next-line no-unused-vars
+function mouseWheel({ delta }) {
+  mouseZ += map(delta, 0, 1500, 0, 500);
+}
+
+// eslint-disable-next-line no-unused-vars
 function draw() {
   clear();
   background(0);
   drawFrameRate();
-  blendMode(ADD);
+
+  push();
+  rectMode(CENTER);
+  noStroke();
+  fill(255, 0, 0);
+  translate(mouseX - cWidth / 2, mouseY - cHeight / 2, mouseZ);
+  rotateX(angle);
+  rotateY(angle);
+  // rect(0, 0, 150, 150);
+  // box(200, 10, 100);
+  torus(100, 42);
+  pop();
+
+  angle += 0.05;
+
+  // blendMode(ADD);
 
   const force = createVector(0, -0.1);
   const mouseDir = map(mouseX, 0, cWidth, -0.1, 0.1);
