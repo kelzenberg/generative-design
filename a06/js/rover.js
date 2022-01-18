001128a1fbfddd568f91392f8c1c2a795819dc9d
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-01-18 17:04:22 +0100

    a06: convert rover into class

--- a06/js/rover.js
+++ a06/js/rover.js
@@ -1,5 +1,3 @@
-let rover;
-
 /* Mouse: click : toggle pointer lock
        left/right : yaw
        up/down : pitch
@@ -27,48 +25,53 @@ const helpTexts = [
 ];
 
 // eslint-disable-next-line no-unused-vars
-function createRover() {
-  rover = createRoverCam();
-  rover.usePointerLock();
+class Rover {
+  constructor() {
+    this.cam = createRoverCam();
+    this.cam.usePointerLock();
 
-  rover.setState({
-    active: true,
-    enableControl: true,
-    // position: [0, 0, 200], // ➡️ ⬇️ ⤵️
-    position: [-250, -25, 125], // ➡️ ⬇️ ⤵️
-    // rotation: [radians(-90), radians(0), 0], // pan, tilt, undefined
-    rotation: [radians(-30), radians(35), 0], // pan, tilt, undefined
-    // offset: [0, 0], // height, rotational
-    offset: [-120, 0], // height, rotational
-    fov: 1, // default: 1
-    speed: 0.5,
-    sensitivity: 0.05,
-  });
-}
-
-// eslint-disable-next-line no-unused-vars
-function drawHelp() {
-  textFont(f);
-  push();
-
-  // taken & modified from https://editor.p5js.org/jwdunn1/sketches/iI-2XX0Hw
-  // credit to James Dunn (https://github.com/jwdunn1)
-  camera(0, 0, height / 2.0 / tan((PI * 30.0) / 180.0), 0, 0, 0, 0, 1, 0);
-  ortho(-width / 2, width / 2, -height / 2, height / 2, 0, 1000);
-  translate(-width / 2 + 20, -height / 2 + 20, 0);
-  scale(1.5);
-  // (credit end)
-
-  const framerate = frameRate().toFixed();
-  if (framerate % 5 == 0) {
-    helpTexts[helpTexts.length - 1] = `[ Framerate: ${framerate} ]`;
+    this.cam.setState({
+      active: true,
+      enableControl: true,
+      // position: [0, 0, 200], // ➡️ ⬇️ ⤵️
+      position: [-250, -25, 125], // ➡️ ⬇️ ⤵️
+      // rotation: [radians(-90), radians(0), 0], // pan, tilt, undefined
+      rotation: [radians(-30), radians(35), 0], // pan, tilt, undefined
+      // offset: [0, 0], // height, rotational
+      offset: [-120, 0], // height, rotational
+      fov: 1, // default: 1
+      speed: 0.5,
+      sensitivity: 0.05,
+    });
   }
 
-  fill(0, 0, 0, 200);
-  noStroke();
-  rect(0, 0, 155, helpTexts.length * 15 + 10);
+  getCam() {
+    return this.cam;
+  }
 
-  fill(127);
-  helpTexts.forEach((v, idx) => text(v, 5, (idx + 1) * 15));
-  pop();
+  drawHelp() {
+    push();
+
+    // taken & modified from https://editor.p5js.org/jwdunn1/sketches/iI-2XX0Hw
+    // credit to James Dunn (https://github.com/jwdunn1)
+    camera(0, 0, height / 2.0 / tan((PI * 30.0) / 180.0), 0, 0, 0, 0, 1, 0);
+    ortho(-width / 2, width / 2, -height / 2, height / 2, 0, 1000);
+    translate(-width / 2 + 20, -height / 2 + 20, 0);
+    scale(1.5);
+    // (credit end)
+
+    const framerate = frameRate().toFixed();
+    if (framerate % 5 == 0) {
+      helpTexts[helpTexts.length - 1] = `[ Framerate: ${framerate} ]`;
+    }
+
+    fill(0, 0, 0, 180);
+    noStroke();
+    rect(0, 0, 155, helpTexts.length * 15 + 10);
+
+    fill(220);
+    helpTexts.forEach((v, idx) => text(v, 5, (idx + 1) * 15));
+
+    pop();
+  }
 }
--- a06/js/sketch.js
+++ a06/js/sketch.js
@@ -4,6 +4,8 @@ let cHeight = 500; // canvas height
 let showHelp = false;
 console.log('Show the help by pressing "h" on your keyboard or by setting "showHelp = true" in the console.');
 
+let rover;
+let cam;
 const flock = [];
 
 // eslint-disable-next-line no-unused-vars
@@ -16,7 +18,10 @@ function setup() {
   cWidth = windowWidth;
   cHeight = windowHeight;
   createCanvas(cWidth, cHeight, WEBGL);
-  createRover();
+  textFont(f);
+
+  rover = new Rover();
+  cam = rover.getCam();
 
   for (let idx = 0; idx < 100; idx++) {
     flock.push(new Boid(random(cWidth), random(cHeight)));
@@ -30,11 +35,13 @@ function keyPressed() {
 
 // eslint-disable-next-line no-unused-vars
 function draw() {
-  background('blue');
+  background(230);
 
   if (showHelp) {
-    drawHelp();
+    rover.drawHelp();
   }
+
+  ambientLight(255, 255, 255);
 
   push();
   noStroke();
